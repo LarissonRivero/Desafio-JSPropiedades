0da4a845e903f9b585ddcf2e8a208569856a4376
LarissonRivero <Larissonrivero2@gmail.com>
2023-01-08 15:57:55 -0300

    Desafio Propiedades

---
+++ assets/js/index.js
@@ -0,0 +1,92 @@
+const propiedadesJSON = [
+    {
+      name: "Casa de campo",
+      description: "Un lugar ideal para descansar de la ciudad",
+      src:
+        "https://www.construyehogar.com/wp-content/uploads/2020/02/Dise%C3%B1o-casa-en-ladera.jpg",
+      rooms: 2,
+      m: 170
+    },
+    {
+      name: "Casa de playa",
+      description: "Despierta tus días oyendo el oceano",
+      src:
+        "https://media.chvnoticias.cl/2018/12/casas-en-la-playa-en-yucatan-2712.jpg",
+      rooms: 2,
+      m: 130
+    },
+    {
+      name: "Casa en el centro",
+      description: "Ten cerca de ti todo lo que necesitas",
+      src:
+        "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+      rooms: 1,
+      m: 80
+    },
+    {
+      name: "Casa rodante",
+      description: "Conviertete en un nómada del mundo",
+      src:
+        "https://cdn.bioguia.com/embed/3d0fb0142790e6b90664042cbafcb1581427139/furgoneta.jpg",
+      rooms: 1,
+      m: 6
+    },
+    {
+      name: "Departamento",
+      description: "Desde las alturas todo se ve mejor",
+      src:
+        "https://www.adondevivir.com/noticias/wp-content/uploads/2016/08/depto-1024x546.jpg",
+      rooms: 3,
+      m: 200
+    },
+    {
+      name: "Mansión",
+      description: "Vive una vida lujosa en una mansión",
+      src:
+        "https://resizer.glanacion.com/resizer/fhK-tSVag_8UGJjPMgWrspslPoU=/768x0/filters:quality(80)/cloudfront-us-east-1.images.arcpublishing.com/lanacionar/CUXVMXQE4JD5XIXX4X3PDZAVMY.jpg",
+      rooms: 5,
+      m: 500
+    }
+  ];
+  const filtrarP = () => {
+    const cCuartos = document.getElementById("cCuartos").value;
+    const mCuarto = document.getElementById("metrosCuarto").value;
+    const hCuarto = document.getElementById("hastaCuarto").value;
+    if (cCuartos && mCuarto && hCuarto) {
+      mostrarProp(cCuartos, mCuarto, hCuarto);
+    } else {
+      alert('Debe llenar todos los campos');
+    }
+  };
+
+  const mostrarProp = (rooms = '*', mc = '*', hc = '*') =>{
+    let card = document.querySelector('.propiedades');
+      let propiedades = '';
+      let total = 0;
+    
+      for (const propiedad of propiedadesJSON){
+        if (
+            (propiedad.rooms == rooms && propiedad.m >= mc && propiedad.m <= hc) ||
+            (rooms == '*' && mc == '*' && hc == '*')
+            ){
+      propiedades += `
+      <div class= "propiedad">
+        <div class="img" style="background-image: url('${propiedad.src}')"></div>
+        <section>
+            <h5>${propiedad.name}</h5>
+            <div class="d-flex justify-content-between">
+                <p>Cuartos: ${propiedad.rooms}</p>
+                <p>Metros: 170</p>
+            </div>
+            <p class="my-3">${propiedad.description}</p>
+            <button class="btn btn-info ">Ver más</button>
+        </section>
+      </div>`;
+    total++;
+    }
+  }
+  card.innerHTML = propiedades;
+  document.querySelector('#totalCuartos').innerHTML = total;
+}
+mostrarProp();
+  
